1283463c870ded53c5d9b9419e296424ecf2ad1e
Tasteofliar <Nightza00777@gmail.com>
2023-11-13 06:10:21 +0700

    mod: add yourtaskFunc.py back

---
+++ src/yourtaskFunc.py
@@ -0,0 +1,16 @@
+def yourtask():
+    task_num = input("task number(1,2,3): ")
+    if task_num == "1":
+        task = "study git"
+        print(task)
+    if task_num == "2":
+        task = "study sphinx & personal sphinx project"
+        print(task)
+    if task_num == "3":
+        task = "learn Ammonite source code"
+        print(task)
+
+yourtask()
+        
+
+    
