d14c8556cd268b82600b8fe84e70d7e03adc5eb1
Tasteofliar <Nightza00777@gmail.com>
2023-11-13 06:53:15 +0700

    mod: add Docstrings in yourtaskFunc

--- src/yourtaskFunc.py
+++ src/yourtaskFunc.py
@@ -1,4 +1,50 @@
 def yourtask():
+    '''This function tell your remaining task, type the task number that you want to know about it"
+
+    :returns: your task.
+    :rtype: str
+
+    Code
+
+    .. code-block:: python
+    
+        def yourtask():
+            task_num = input("task number(1,2,3): ")
+            if task_num == "1":
+                task = "study git"
+                print(task)
+            if task_num == "2":
+                task = "study sphinx & personal sphinx project"
+                print(task)
+            if task_num == "3":
+                task = "learn Ammonite source code"
+                print(task)
+
+    Example
+
+    .. code-block:: python
+
+        task number(1,2,3): 1
+
+    Output
+
+    .. code-block:: python
+
+        study git
+
+    Task number & Return statement
+
+    +-------------+---------------------------------------+
+    | Task number |            return statement           |        
+    +=============+=======================================+
+    |      1      |                study git              |
+    +-------------+---------------------------------------+
+    |      2      | study sphinx & personal sphinx project|
+    +-------------+---------------------------------------+
+    |      3      |       learn Ammonite source code      |
+    +-------------+---------------------------------------+
+
+    '''
     task_num = input("task number(1,2,3): ")
     if task_num == "1":
         task = "study git"
@@ -10,7 +56,5 @@ def yourtask():
         task = "learn Ammonite source code"
         print(task)
 
-yourtask()
-        
 
     
