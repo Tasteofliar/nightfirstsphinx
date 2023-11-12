5f36a3dc59401f9e92b54403175d82be511303ce
Tasteofliar <Nightza00777@gmail.com>
2023-11-13 06:31:28 +0700

    mod: add Docstrings in yourinfoFunc

--- src/yourinfoFunc.py
+++ src/yourinfoFunc.py
@@ -1,4 +1,29 @@
 def yourinfo(name,age,gender):
+    '''This function give me your information, so please fill in the information truthfully ;)
+
+    :param str name: input your name.
+    :param int age: input your age.
+    :param str name: input your gender.
+
+    :returns: your information.
+    :rtype: str
+
+    Example
+
+    .. code-block:: python
+
+        myinfo = yourinfo("Nontaphat","20","Male")
+        print(myinfo)
+
+    Output
+
+    .. code-block:: python
+
+        name: Nontaphat
+        age: 20
+        gender: Male
+
+    '''
     profile = "name: {}\nage: {}\ngender: {}".format(name,age,gender)
     return profile
 
