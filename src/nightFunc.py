248ea2fe8fe918a7c031223c0a9af74d0fdc7af3
Tasteofliar <Nightza00777@gmail.com>
2023-11-13 06:24:28 +0700

    mod: add Docstrings in nightFunc

--- src/nightFunc.py
+++ src/nightFunc.py
@@ -1,6 +1,24 @@
 def nightfunc(name):
+    '''Greeting function for this module, tell me what your name and i'm gonna greet you!
+
+    :param str name: input your name.
+
+    :returns: greeting word.
+    :rtype: str
+
+    Example
+
+    .. code-block:: python
+
+        myGreet = nightfunc(name='Nontaphat')
+        print(myGreet)
+
+    Output
+
+    .. code-block:: python
+
+        "Hello Nontaphat!. My name is Night. Nice to meet you!"
+
+    '''
     greet = "Hello {}!. My name is Night. Nice to meet you!".format(name)
     return greet
-
-greeting = nightfunc("pai")
-print(greeting)
