0a0cfe7c2505b361e581c7ef7b258a427a211f24
Tasteofliar <Nightza00777@gmail.com>
2023-11-13 07:06:30 +0700

    mod: remove Code example in yourtaskFunc

--- src/yourtaskFunc.py
+++ src/yourtaskFunc.py
@@ -4,22 +4,6 @@ def yourtask():
     :returns: your task.
     :rtype: str
 
-    Code
-
-    .. code-block:: python
-    
-        def yourtask():
-            task_num = input("task number(1,2,3): ")
-            if task_num == "1":
-                task = "study git"
-                print(task)
-            if task_num == "2":
-                task = "study sphinx & personal sphinx project"
-                print(task)
-            if task_num == "3":
-                task = "learn Ammonite source code"
-                print(task)
-
     Example
 
     .. code-block:: python
